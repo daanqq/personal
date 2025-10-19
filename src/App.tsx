94dcd999d4fa99a8fc46741bed8890ae65896ecf
Danila Andreev <qq@daanqq.ru>
2025-10-19 16:32:16 +0500

    set up minimal screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import ASCIIText from "./components/ASCIIText"
+import FaultyTerminal from "./components/FaultyTerminal"
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className='w-full h-screen'>
+      <FaultyTerminal
+        scale={3}
+        gridMul={[2, 1]}
+        digitSize={1.2}
+        timeScale={1}
+        pause={false}
+        scanlineIntensity={1}
+        glitchAmount={1}
+        flickerAmount={1}
+        noiseAmp={1}
+        chromaticAberration={0}
+        dither={0.2}
+        curvature={0.05}
+        tint="#230046"
+        mouseReact={true}
+        mouseStrength={0.5}
+        pageLoadAnimation={false}
+        brightness={1}
+      >
+        <ASCIIText
+          text='andreev danila'
+          // enableWaves={true}
+          asciiFontSize={10}
+          planeBaseHeight={4}
+        />
+      </FaultyTerminal>
+    </div>
   )
 }
 
